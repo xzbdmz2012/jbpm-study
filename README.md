59f6eb50a603440f26638bae726e74faad1e9ca3
3136803133@qq,com <zxbdmz2012>
2024-03-05 08:52:05 +0800

    init

--- README.md
+++
@@ -1,3 +0,0 @@
-# study-jbpm
-
-jBPM7.15 learn。document URL：https://docs.jboss.org/jbpm/release/latest/jbpm-docs/html_single/index.html#_jbpminstaller
--- src/main/java/com/example/qj/demo/exception/MyError.java
+++
@@ -1,7 +0,0 @@
-package com.example.qj.demo.exception;
-
-public class MyError extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-
-}
